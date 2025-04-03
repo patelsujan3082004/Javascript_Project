388729aeff6d9d40129f1f0af2b1aae82020ed5c
Sujan Patel <157210759+patelsujan3082004@users.noreply.github.com>
2025-04-03 10:36:32 +0530

    Add files via upload

---
+++ P-4/Remove.js
@@ -0,0 +1,8 @@
+// Remove Duplicates
+
+
+function remove(arr) {   
+    const unique = new Set(arr); 
+    return Array.from(unique); 
+}
+console.log(remove([1, 2, 3, 4, 5, 1, 2, 3])); 
---
+++ P-4/unique.js
@@ -0,0 +1,17 @@
+// => find unique element from array\
+
+
+let arr = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]; 
+let unique = []; 
+arr.forEach((item) => { 
+    a=0;
+    unique.forEach((i)=>{
+        if(item===i){
+            a++;
+        }
+    });
+    if(a===0){
+        unique.push(item);
+    }
+});
+console.log(unique); 
